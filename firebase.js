141512cbf1f99430889b07c64f23a7872f6effbf
Jarudech-Chop <jarudech.thab@gmail.com>
2026-06-05 10:07:56 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCfHgobXDmzGD6bZAeSRS4vAnCnoXU3KYE",
+  authDomain: "manday-record.firebaseapp.com",
+  projectId: "manday-record",
+  storageBucket: "manday-record.firebasestorage.app",
+  messagingSenderId: "744520485540",
+  appId: "1:744520485540:web:60f08e9995ec2cf9c07431",
+  measurementId: "G-H60NW9HNX5"
 };
 
 const app = initializeApp(firebaseConfig);

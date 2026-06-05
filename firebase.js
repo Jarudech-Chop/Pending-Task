6aa05dfec5209d976296d9e866ab0b4dd3f056b2
Jarudech-Chop <jarudech.thab@gmail.com>
2026-06-05 10:04:05 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const collectionName = "projectTasks";
+
+export { db, collectionName };
